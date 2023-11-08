d3e295fe553fc04c75663117f774c87955628461
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2023-11-08 12:43:56 +0000

    Update post-install.bash

--- proxmox/post-install.bash
+++ proxmox/post-install.bash
@@ -15,7 +15,8 @@ rm /etc/apt/sources.list.d/pve-enterprise.list
 echo 'deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription' > /etc/apt/sources.list.d/ceph-no-subscription.list
 echo 'deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription' > /etc/apt/sources.list.d/pve-no-subscription.list
 
-echo 'deb [signed-by=/usr/share/keyrings/tailscale-archive-keyring.gpg] https://pkgs.tailscale.com/stable/debian bookworm main' > /etc/apt/sources.list.d/tailscale.list
+curl --fail --location --output /etc/apt/sources.list.d/tailscale.list --show-error --silent 'https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list'
+curl --fail --location --output /usr/share/keyrings/tailscale-archive-keyring.gpg --show-error --silent 'https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg'
 
 apt-get update
 apt-get upgrade
