3eb0bb5ee883282b425f40ad865161f3e9d06ec3
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2023-12-07 14:55:57 +0000

    Update configure.bash

--- kodi/configure.bash
+++ kodi/configure.bash
@@ -25,42 +25,28 @@ cat << EOF > "${KODI}/userdata/guisettings.xml"
 </settings>
 EOF
 
+cat << EOF > "${KODI}/userdata/mediasources.xml"
+<mediasources>
+    <network>
+        <location id="0">${SERVER}/media</location>
+        <location id="1">${SERVER}/rips</location>
+    </network>
+</mediasources>
+EOF
+
 cat << EOF > "${KODI}/userdata/sources.xml"
 <sources>
-    <files>
-        <default pathversion="1"></default>
-    </files>
-    <games>
-        <default pathversion="1"></default>
-    </games>
-    <music>
-        <default pathversion="1"></default>
-    </music>
-    <pictures>
-        <default pathversion="1"></default>
-    </pictures>
-    <programs>
-        <default pathversion="1"></default>
-    </programs>
     <video>
         <default pathversion="1"></default>
         <source>
-            <name>media - movies</name>
+            <name>movies</name>
             <path pathversion="1">${SERVER}/media/movies/</path>
-            <allowsharing>false</allowsharing>
-        </source>
-        <source>
-            <name>media - television</name>
-            <path pathversion="1">${SERVER}/media/television/</path>
-            <allowsharing>false</allowsharing>
-        </source>
-        <source>
-            <name>rips - movies</name>
             <path pathversion="1">${SERVER}/rips/movies/</path>
             <allowsharing>false</allowsharing>
         </source>
         <source>
-            <name>rips - television</name>
+            <name>television</name>
+            <path pathversion="1">${SERVER}/media/television/</path>
             <path pathversion="1">${SERVER}/rips/television/</path>
             <allowsharing>false</allowsharing>
         </source>
