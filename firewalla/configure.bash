4bb4e353f2824cec1c1beee0ac185efeeee3b6f2
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2024-01-08 22:10:54 +0000

    Update configure.bash

--- firewalla/configure.bash
+++ firewalla/configure.bash
@@ -7,8 +7,7 @@ mkdir "${HOME}/.ssh"
 
 chmod 0700 "${HOME}/.ssh"
 
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKYDHpVs4nKaLG+tnLUGH+4Ivnq9ELPW0S
-3W/uJhxNd/' > "${HOME}/.ssh/authorized_keys"
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKYDHpVs4nKaLG+tnLUGH+4Ivnq9ELPW0S3W/uJhxNd/' > "${HOME}/.ssh/authorized_keys"
 
 chmod 0400 "${HOME}/.ssh/authorized_keys"
 chmod 0500 "${HOME}/.ssh"
