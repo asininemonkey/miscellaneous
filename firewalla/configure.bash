5c6e2afa98433e82f1c8587d32f013de0fbf978d
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2024-01-17 11:49:55 +0000

    Update configure.bash

--- firewalla/configure.bash
+++ firewalla/configure.bash
@@ -3,10 +3,9 @@
 unalias apt
 unalias apt-get
 
-sudo systemctl enable --now docker
+# sudo systemctl enable --now docker
 
 curl -fsSL 'https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list' | sudo tee '/etc/apt/sources.list.d/tailscale.list' > /dev/null
-
 curl -fsSL 'https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg' | sudo tee '/usr/share/keyrings/tailscale-archive-keyring.gpg' > /dev/null
 
 sudo apt-get update
