102e427b86a30dcf171385a0d2efce60ac356b30
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2024-05-04 16:47:27 +0100

    Update configure.bash

--- kodi/configure.bash
+++ kodi/configure.bash
@@ -3,9 +3,21 @@
 set -x
 
 KODI="${HOME}/.kodi"
-SERVER="nfs://192.168.144.200/data"
+MOUNT="/mnt/intel-nuc"
+SERVER="192.168.144.200"
 
 systemctl stop mediacenter.service
+
+sudo apt-get install --yes autofs
+
+echo '/- /etc/auto.nfs --timeout 5 browse' | sudo tee /etc/auto.master.d/nfs.autofs
+
+cat << EOF | sudo tee /etc/auto.nfs
+${MOUNT}/media/movies     ${SERVER}:/data/media/movies
+${MOUNT}/media/television ${SERVER}:/data/media/television
+${MOUNT}/rips/movies      ${SERVER}:/data/rips/movies
+${MOUNT}/rips/television  ${SERVER}:/data/rips/television
+EOF
 
 rm --force --recursive "${KODI}"
 
@@ -28,28 +40,20 @@ cat << EOF > "${KODI}/userdata/guisettings.xml"
 </settings>
 EOF
 
-cat << EOF > "${KODI}/userdata/mediasources.xml"
-<mediasources>
-    <network>
-        <location id="0">${SERVER}</location>
-    </network>
-</mediasources>
-EOF
-
 cat << EOF > "${KODI}/userdata/sources.xml"
 <sources>
     <video>
         <default pathversion="1"></default>
         <source>
             <name>movies</name>
-            <path pathversion="1">${SERVER}/media/movies/</path>
-            <path pathversion="1">${SERVER}/rips/movies/</path>
+            <path pathversion="1">${MOUNT}/media/movies/</path>
+            <path pathversion="1">${MOUNT}/rips/movies/</path>
             <allowsharing>false</allowsharing>
         </source>
         <source>
             <name>television</name>
-            <path pathversion="1">${SERVER}/media/television/</path>
-            <path pathversion="1">${SERVER}/rips/television/</path>
+            <path pathversion="1">${MOUNT}/media/television/</path>
+            <path pathversion="1">${MOUNT}/rips/television/</path>
             <allowsharing>false</allowsharing>
         </source>
     </video>
@@ -60,9 +64,9 @@ systemctl start mediacenter.service
 
 sleep 10
 
-sqlite3 "${KODI}/userdata/Database/MyVideos121.db" "INSERT INTO 'path' VALUES (1,'${SERVER}/media/movies/','movies','metadata.themoviedb.org.python',NULL,2147483647,1,NULL,0,0,0,NULL,NULL);"
-sqlite3 "${KODI}/userdata/Database/MyVideos121.db" "INSERT INTO 'path' VALUES (2,'${SERVER}/rips/movies/','movies','metadata.themoviedb.org.python',NULL,2147483647,1,NULL,0,0,0,NULL,NULL);"
-sqlite3 "${KODI}/userdata/Database/MyVideos121.db" "INSERT INTO 'path' VALUES (3,'${SERVER}/media/television/','tvshows','metadata.tvshows.themoviedb.org.python',NULL,0,0,NULL,0,0,0,NULL,NULL);"
-sqlite3 "${KODI}/userdata/Database/MyVideos121.db" "INSERT INTO 'path' VALUES (4,'${SERVER}/rips/television/','tvshows','metadata.tvshows.themoviedb.org.python',NULL,0,0,NULL,0,0,0,NULL,NULL);"
+sqlite3 "${KODI}/userdata/Database/MyVideos121.db" "INSERT INTO 'path' VALUES (1,'${MOUNT}/media/movies/','movies','metadata.themoviedb.org.python',NULL,2147483647,1,NULL,0,0,0,NULL,NULL);"
+sqlite3 "${KODI}/userdata/Database/MyVideos121.db" "INSERT INTO 'path' VALUES (2,'${MOUNT}/rips/movies/','movies','metadata.themoviedb.org.python',NULL,2147483647,1,NULL,0,0,0,NULL,NULL);"
+sqlite3 "${KODI}/userdata/Database/MyVideos121.db" "INSERT INTO 'path' VALUES (3,'${MOUNT}/media/television/','tvshows','metadata.tvshows.themoviedb.org.python',NULL,0,0,NULL,0,0,0,NULL,NULL);"
+sqlite3 "${KODI}/userdata/Database/MyVideos121.db" "INSERT INTO 'path' VALUES (4,'${MOUNT}/rips/television/','tvshows','metadata.tvshows.themoviedb.org.python',NULL,0,0,NULL,0,0,0,NULL,NULL);"
 
 kodi-send --action "UpdateLibrary(video)"
