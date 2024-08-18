02b9a75537077aeee91aaaedfab65323b88c8dd0
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2024-08-18 19:25:45 +0100

    Repository Update

---
+++ linux/intel-nuc/install.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+archinstall --config 'http://192.168.144.210:5000/co.json' --creds 'https://raw.githubusercontent.com/asininemonkey/miscellaneous/main/linux/intel-nuc/creds.json' --silent --skip-version-check
