e7567aca8b9e1277a953136558eb7b2c9a7822e2
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-03-02 20:17:47 +0000

    Repository Update

---
+++ linux/asrock/install.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+archinstall \
+    --config 'https://raw.githubusercontent.com/asininemonkey/miscellaneous/main/linux/asrock/config.json' \
+    --creds 'https://raw.githubusercontent.com/asininemonkey/miscellaneous/main/linux/asrock/creds.json' \
+    --silent \
+    --skip-version-check
