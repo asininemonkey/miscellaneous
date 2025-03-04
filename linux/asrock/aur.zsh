c2853b5a14392ed02146f0318de2ee4732f317a2
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-03-04 18:32:06 +0000

    Update aur.zsh

--- linux/asrock/aur.zsh
+++ linux/asrock/aur.zsh
@@ -1,7 +1,13 @@
 #!/usr/bin/env zsh
 
-curl --location --output /tmp/paru.tar.zst https://github.com/morganamilo/paru/releases/download/v2.0.4/paru-v2.0.4-x86_64.tar.zst
+#curl --location --output /tmp/paru.tar.zst https://github.com/morganamilo/paru/releases/download/v2.0.4/paru-v2.0.4-x86_64.tar.zst
 
-tar --directory /tmp --extract --file /tmp/paru.tar.zst --zstd paru
+curl --location --output /tmp/yay.tar.gz https://github.com/Jguer/yay/releases/download/v12.4.2/yay_12.4.2_x86_64.tar.gz
 
-su jcardoso --command '/tmp/paru --noconfirm --refresh --sync 1password 1password-cli paru'
+#tar --directory /tmp --extract --file /tmp/paru.tar.zst --zstd paru
+
+tar --directory /tmp --extract --file /tmp/yay.tar.gz --gzip yay
+
+#su jcardoso --command '/tmp/paru --noconfirm --refresh --sync 1password 1password-cli paru'
+
+su jcardoso --command '/tmp/yay --noconfirm --refresh --sync yay'
