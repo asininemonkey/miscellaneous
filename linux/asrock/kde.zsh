fb67072ff932238d85f24094f91aeee98f3711a3
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-03-09 14:53:39 +0000

    Update kde.zsh

--- linux/asrock/kde.zsh
+++ linux/asrock/kde.zsh
@@ -16,6 +16,23 @@ GreeterEnvironment=QT_WAYLAND_SHELL_INTEGRATION=layer-shell
 CompositorCommand=kwin_wayland --drm --locale1 --no-global-shortcuts --no-lockscreen
 EOF
 
+cat << 'EOF' > /home/jcardoso/.config/ghostty/config
+# https://ghostty.org/docs/config/reference
+app-notifications = no-clipboard-copy
+background-opacity = 0.85
+bold-is-bright = true
+clipboard-paste-protection = true
+copy-on-select = clipboard
+cursor-style = block
+cursor-style-blink = true
+font-family = Iosevka Nerd Font
+font-size = 14
+keybind = ctrl+shift+k=clear_screen
+theme = Builtin Tango Dark
+window-height = 35
+window-width = 155
+EOF
+
 cat << 'EOF' > /home/jcardoso/.config/kdeglobals
 [Icons]
 Theme=Papirus-Dark
@@ -76,4 +93,19 @@ cat << 'EOF' > /home/jcardoso/.config/kwinoutputconfig.json
         "name": "setups"
     }
 ]
+EOF
+
+cat << 'EOF' > /home/jcardoso/.local/share/user-places.xbel
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE xbel>
+<xbel>
+ <bookmark href="davs://files.josecardoso.net/">
+  <title>files.josecardoso.net</title>
+  <info>
+   <metadata owner="http://freedesktop.org">
+    <bookmark:icon name="folder-yellow-network"/>
+   </metadata>
+  </info>
+ </bookmark>
+</xbel>
 EOF
