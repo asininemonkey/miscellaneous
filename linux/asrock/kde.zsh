028bb5201fc5b796fc40de48378432aae7dd920a
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-03-09 15:12:25 +0000

    Update kde.zsh

--- linux/asrock/kde.zsh
+++ linux/asrock/kde.zsh
@@ -16,23 +16,6 @@ GreeterEnvironment=QT_WAYLAND_SHELL_INTEGRATION=layer-shell
 CompositorCommand=kwin_wayland --drm --locale1 --no-global-shortcuts --no-lockscreen
 EOF
 
-cat << 'EOF' > /home/jcardoso/.config/ghostty/config
-# https://ghostty.org/docs/config/reference
-app-notifications = no-clipboard-copy
-background-opacity = 0.85
-bold-is-bright = true
-clipboard-paste-protection = true
-copy-on-select = clipboard
-cursor-style = block
-cursor-style-blink = true
-font-family = Iosevka Nerd Font
-font-size = 14
-keybind = ctrl+shift+k=clear_screen
-theme = Builtin Tango Dark
-window-height = 35
-window-width = 155
-EOF
-
 cat << 'EOF' > /home/jcardoso/.config/kdeglobals
 [Icons]
 Theme=Papirus-Dark
