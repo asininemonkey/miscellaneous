2b8dcb6d1940a4fe0e9f6f030c791100f0004951
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-04-28 17:26:12 +0100

    Update aur.zsh

--- linux/arch/asrock/aur.zsh
+++ linux/arch/asrock/aur.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-curl --location --output /tmp/yay.tar.gz https://github.com/Jguer/yay/releases/download/v12.4.2/yay_12.4.2_x86_64.tar.gz
+curl --location --output /tmp/yay.tar.gz https://github.com/Jguer/yay/releases/download/v12.5.0/yay_12.5.0_x86_64.tar.gz
 
 tar --directory /tmp --extract --file /tmp/yay.tar.gz --gzip --no-anchored --strip-components 1 yay
 
-su jcardoso --command '/tmp/yay --noconfirm --refresh --sync 1password 1password-cli octopi oh-my-posh-bin sublime-merge yay-bin'
+su jcardoso --command '/tmp/yay --noconfirm --refresh --sync 1password 1password-cli localsend-bin octopi oh-my-posh-bin sublime-merge yay-bin'
