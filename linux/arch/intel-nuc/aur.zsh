3e70f04655c3ef309fac1f081940d11a5ee574b6
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-04-28 17:26:54 +0100

    Update aur.zsh

--- linux/arch/intel-nuc/aur.zsh
+++ linux/arch/intel-nuc/aur.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-curl --location --output /tmp/yay.tar.gz https://github.com/Jguer/yay/releases/download/v12.4.2/yay_12.4.2_x86_64.tar.gz
+curl --location --output /tmp/yay.tar.gz https://github.com/Jguer/yay/releases/download/v12.5.0/yay_12.5.0_x86_64.tar.gz
 
 tar --directory /tmp --extract --file /tmp/yay.tar.gz --gzip --no-anchored --strip-components 1 yay
 
