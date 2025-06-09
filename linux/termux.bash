a5f55c55ee8f6021e0e558e35f48993ec3291ceb
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-06-09 16:38:24 +0100

    Update termux.bash

--- linux/termux.bash
+++ linux/termux.bash
@@ -9,3 +9,18 @@ pkg install --yes \
     openssh \
     tree \
     wget
+
+mkdir --mode 0700 ~/.ssh ~/.termux
+
+touch ~/.ssh/id_ed25519 ~/.ssh/id_ed25519.pub
+
+chmod 0400 ~/.ssh/id_*
+
+cat << EOF > ~/.termux/termux.properties
+# https://github.com/termux/termux-tools/blob/master/termux.properties
+extra-keys = []
+hide-soft-keyboard-on-startup = true
+terminal-cursor-blink-rate = 750
+EOF
+
+termux-reload-settings
