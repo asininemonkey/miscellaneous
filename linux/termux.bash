04c2f9c8675ed0e8823e531c88305bdf32770426
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-06-11 15:36:04 +0100

    Update termux.bash

--- linux/termux.bash
+++ linux/termux.bash
@@ -26,18 +26,18 @@ pkg install --yes \
 
 chsh -s zsh
 
-if [ ! -f ~/.termux/font.ttf ]; then
-    curl --location --output ~/.termux/font.ttf https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/IosevkaTerm/IosevkaTermNerdFont-Regular.ttf
-fi
-
-if [ ! -f ~/.ssh/authorized_keys ]; then
-cat << 'EOF' > ~/.ssh/authorized_keys
+if [ ! -s ~/.ssh/authorized_keys ]
+then
+    cat << 'EOF' > ~/.ssh/authorized_keys
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQowLl5Bzn87ig+Gs7Ze5kWODRTdHiD+V8sOCwOx16Z
 EOF
+
+    chmod 0400 ~/.ssh/authorized_keys
 fi
 
-if [ ! -f ~/.ssh/config ]; then
-cat << 'EOF' > ~/.ssh/config
+if [ ! -f ~/.ssh/config ]
+then
+    cat << 'EOF' > ~/.ssh/config
 Host *
     User jcardoso
 
@@ -50,10 +50,23 @@ Host openmediavault
 Host raspberry-pi-5-16gb-docker
     HostName 192.168.144.2
 EOF
+
+    chmod 0400 ~/.ssh/config
+fi
+
+if [ ! -f ~/.ssh/id_ed25519 ]
+then
+    touch ~/.ssh/id_ed25519 ~/.ssh/id_ed25519.pub
+
+    chmod 0400 ~/.ssh/id_*
+fi
+
+if [ ! -f ~/.termux/font.ttf ]
+then
+    curl --location --output ~/.termux/font.ttf https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/IosevkaTerm/IosevkaTermNerdFont-Regular.ttf
 fi
 
 cat << 'EOF' > ~/.termux/termux.properties
-# https://github.com/termux/termux-tools/blob/master/termux.properties
 extra-keys = []
 hide-soft-keyboard-on-startup = true
 terminal-cursor-blink-rate = 750
@@ -63,9 +76,5 @@ cat << 'EOF' > ~/.zshrc
 autoload -U compinit && compinit
 eval "$(oh-my-posh init zsh)"
 EOF
-
-touch ~/.ssh/id_ed25519 ~/.ssh/id_ed25519.pub
-
-chmod 0400 ~/.ssh/authorized_keys ~/.ssh/config ~/.ssh/id_*
 
 termux-reload-settings
