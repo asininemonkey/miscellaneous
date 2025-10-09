51b5ee1ed9833ce339447fc9b174118cba72c561
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-10-09 15:04:34 +0100

    Add decman

---
+++ linux/arch/decman/source.py
@@ -0,0 +1,49 @@
+# https://github.com/kiviktnm/decman/blob/main/example/source.py
+
+import decman
+import re
+import subprocess
+
+from decman import File
+
+lscpu = subprocess.run(
+    ["lscpu"],
+    capture_output=True,
+    check=True,
+    text=True,
+)
+
+decman.aur_packages += ["decman"]
+
+decman.enabled_systemd_units += [
+    "docker.service",
+    "sshd.service",
+]
+
+decman.files["/etc/vconsole.conf"] = File(
+    owner="root", permissions=0o644, source_file="files/etc/vconsole.conf"
+)
+
+decman.ignored_packages += [
+    "base",
+    "grub",
+    "linux",
+    "linux-firmware",
+    "yay-bin",
+    "yay-bin-debug",
+    "zram-generator",
+]
+
+decman.packages += [
+    "base-devel",
+    "btop",
+    "docker",
+    "git",
+    "openssh",
+    "wget",
+]
+
+if re.search("VMware", lscpu.stdout):
+    decman.packages += [
+        "open-vm-tools",
+    ]
