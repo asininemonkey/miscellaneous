f003c86fb988db56e76c3b620c5aca65de9a7cac
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2025-10-23 13:16:01 +0100

    Update bootstrap.bash

--- linux/arch/bootstrap.bash
+++ linux/arch/bootstrap.bash
@@ -2,14 +2,6 @@
 
 set -ex
 
-cat << EOF > /etc/vconsole.conf
-FONT=default8x16
-KEYMAP=uk
-XKBLAYOUT=gb
-XKBMODEL=pc105
-XKBOPTIONS=terminate:ctrl_alt_bksp
-EOF
-
 sed --in-place 's/ALL=(ALL)/ALL=(ALL) NOPASSWD:/' /etc/sudoers.d/00_jcardoso
 
 sudo --user jcardoso chezmoi init asininemonkey --apply
