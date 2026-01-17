7bcec7b0f8581dc6b5b51012a01de2acd2939671
Jose Cardoso <65740649+asininemonkey@users.noreply.github.com>
2026-01-17 23:36:12 +0000

    Update bootstrap.bash

--- linux/arch/bootstrap.bash
+++ linux/arch/bootstrap.bash
@@ -2,8 +2,8 @@
 
 set -ex
 
-sed --in-place 's/ALL=(ALL)/ALL=(ALL) NOPASSWD:/' /etc/sudoers.d/00_jcardoso
+# sed --in-place 's/ALL=(ALL)/ALL=(ALL) NOPASSWD:/' /etc/sudoers.d/00_jcardoso
 
-# sudo --user jcardoso chezmoi init asininemonkey --apply
+# xxx
 
-sed --in-place 's/NOPASSWD: //' /etc/sudoers.d/00_jcardoso
+# sed --in-place 's/NOPASSWD: //' /etc/sudoers.d/00_jcardoso
